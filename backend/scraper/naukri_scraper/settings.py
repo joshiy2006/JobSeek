a29ac07562f9c493fe3674ae1482fd04283565bf
joshiy2006 <yashvij451@gmail.com>
2026-07-16 22:15:44 +0530

    forgot to import the libraries oops

--- backend/scraper/naukri_scraper/settings.py
+++ backend/scraper/naukri_scraper/settings.py
@@ -7,6 +7,7 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import os
 BOT_NAME = "naukri_scraper"
 
 SPIDER_MODULES = ["naukri_scraper.spiders"]
